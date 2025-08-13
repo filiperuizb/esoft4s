214cc7a080d9c4c8242bc91f2679327ec953c591
Filipe Ruiz Boligon <filiperuizboligon9@gmail.com>
2025-08-13 00:12:38 -0300

    Update ContaBancaria.java

--- Poo-Java/12-08-2025/ContaBancariaSimples/src/ContaBancaria.java
+++ Poo-Java/12-08-2025/ContaBancariaSimples/src/ContaBancaria.java
@@ -4,15 +4,11 @@ import java.util.List;
 public class ContaBancaria {
     private Double saldo;
     private String numeroConta;
-
     private String agencia;
-
     private Pessoa titular;
-
     private List<Cartao> cartoes = new ArrayList<>();
 
-    ContaBancaria(String numeroConta, String agencia, String nomePessoa, String cpf,
-                  String dataNascimento) {
+    public ContaBancaria(String numeroConta, String agencia, String nomePessoa, String cpf, String dataNascimento) {
         this.numeroConta = numeroConta;
         this.agencia = agencia;
         this.titular = new Pessoa(nomePessoa, cpf, dataNascimento);
@@ -26,4 +22,25 @@ public class ContaBancaria {
         cartoes.add(cartao);
     }
 
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("===== DADOS DA CONTA =====\n")
+          .append("Número da Conta: ").append(numeroConta).append("\n")
+          .append("Agência: ").append(agencia).append("\n")
+          .append("Saldo: R$ ").append(saldo).append("\n\n")
+          .append("===== DADOS DO TITULAR =====\n")
+          .append(titular).append("\n\n")
+          .append("===== CARTÕES ASSOCIADOS =====\n");
+
+        if (cartoes.isEmpty()) {
+            sb.append("Nenhum cartão cadastrado.\n");
+        } else {
+            for (int i = 0; i < cartoes.size(); i++) {
+                sb.append("Cartão ").append(i + 1).append(":\n")
+                  .append(cartoes.get(i)).append("\n");
+            }
+        }
+        return sb.toString();
+    }
 }
