4e86287000126efe7379dd925d775055cafc6198
Filipe Ruiz Boligon <filiperuizboligon9@gmail.com>
2025-08-13 00:12:25 -0300

    Update Cartao.java

--- Poo-Java/12-08-2025/ContaBancariaSimples/src/Cartao.java
+++ Poo-Java/12-08-2025/ContaBancariaSimples/src/Cartao.java
@@ -1,17 +1,20 @@
 public class Cartao {
     private String codigo;
-
     private String cvv;
-
     private String validade;
-
     private ContaBancaria contaBancaria;
 
-    Cartao(String codigo, String cvv, String validade, ContaBancaria contaBancaria) {
+    public Cartao(String codigo, String cvv, String validade, ContaBancaria contaBancaria) {
         this.codigo = codigo;
         this.cvv = cvv;
         this.validade = validade;
         this.contaBancaria = contaBancaria;
     }
 
+    @Override
+    public String toString() {
+        return "Código: " + codigo + "\n" +
+               "CVV: " + cvv + "\n" +
+               "Validade: " + validade + "\n";
+    }
 }
