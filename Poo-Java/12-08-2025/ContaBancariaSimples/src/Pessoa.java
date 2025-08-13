0eb4a7a2d1bdc0f0c4cc502ac07e036c42792876
Filipe Ruiz Boligon <filiperuizboligon9@gmail.com>
2025-08-13 00:12:10 -0300

    Update Pessoa.java

--- Poo-Java/12-08-2025/ContaBancariaSimples/src/Pessoa.java
+++ Poo-Java/12-08-2025/ContaBancariaSimples/src/Pessoa.java
@@ -1,14 +1,18 @@
-import java.time.LocalDate;
-
 public class Pessoa {
     private String nome;
     private String cpf;
     private String dataNascimento;
 
-    Pessoa(String nome, String cpf, String dataNascimento) {
+    public Pessoa(String nome, String cpf, String dataNascimento) {
         this.nome = nome;
         this.cpf = cpf;
         this.dataNascimento = dataNascimento;
     }
 
+    @Override
+    public String toString() {
+        return "Nome: " + nome + "\n" +
+               "CPF: " + cpf + "\n" +
+               "Data de Nascimento: " + dataNascimento;
+    }
 }
