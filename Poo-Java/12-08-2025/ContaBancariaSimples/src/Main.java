7dcc592a37703f6f23f9b16200ec3c8287b7ad05
Filipe Ruiz Boligon <filiperuizboligon9@gmail.com>
2025-08-13 00:12:52 -0300

    Update Main.java

--- Poo-Java/12-08-2025/ContaBancariaSimples/src/Main.java
+++ Poo-Java/12-08-2025/ContaBancariaSimples/src/Main.java
@@ -1,11 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        ContaBancaria contaNu = new ContaBancaria("2131321", "213-2",
-        "Filipe", "135.739.259-10", "16/09/2005");
+        ContaBancaria contaNu = new ContaBancaria(
+                "2131321", "213-2", "Filipe", "135.739.259-10", "16/09/2005");
 
         contaNu.setSaldo(3500.00);
-
         contaNu.adicionarCartao(new Cartao("12312321", "123", "08/31", contaNu));
+        contaNu.adicionarCartao(new Cartao("98765432", "999", "05/29", contaNu));
 
+        System.out.println(contaNu);
     }
 }
